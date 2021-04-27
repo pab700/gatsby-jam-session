40fbe73a046554369a6c9985f497ca148238e97a
Paul <pa.brand@web.de>
2021-04-27 20:38:25 +0200

    genres.js added

---
+++ src/pages/randb.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const RandBPage = () => (
+  <Layout>
+    <SEO title="Choose genre" />
+    <h1>R&B.</h1>
+  </Layout>
+)
+
+export default RandBPage
